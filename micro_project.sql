2f60068e43a92ef8d7e12fb5dc970dc86f6cf294
Haika\Hugo Candraditya <hugocandraditya@gmail.com>
2023-05-03 21:57:59 +0700

    update database

--- micro_project.sql
+++ micro_project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 12, 2023 at 10:11 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: May 03, 2023 at 04:57 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,23 +32,53 @@ CREATE TABLE `account` (
   `user_id` int(11) NOT NULL,
   `acc_number` varchar(20) NOT NULL,
   `acc_type` varchar(20) NOT NULL,
-  `acc_name` varchar(40) NOT NULL
+  `acc_name` varchar(40) NOT NULL,
+  `balance` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`id`, `user_id`, `acc_number`, `acc_type`, `acc_name`) VALUES
-(1, 1, '123123123', 'Kredit', 'Hugo ');
+INSERT INTO `account` (`id`, `user_id`, `acc_number`, `acc_type`, `acc_name`, `balance`) VALUES
+(1, 1, '123123123', 'Kredit', 'Hugo ', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `payment_history`
+-- Table structure for table `lending_product`
 --
 
-CREATE TABLE `payment_history` (
+CREATE TABLE `lending_product` (
+  `id` int(11) NOT NULL,
+  `user_id_lender` int(11) DEFAULT NULL,
+  `user_id_borrower` int(11) DEFAULT NULL,
+  `account_lender` varchar(50) DEFAULT NULL,
+  `account_borrower` varchar(50) DEFAULT NULL,
+  `interest` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `total_reimbursement` int(11) DEFAULT NULL,
+  `due_time` date DEFAULT NULL,
+  `reference` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `lending_product`
+--
+
+INSERT INTO `lending_product` (`id`, `user_id_lender`, `user_id_borrower`, `account_lender`, `account_borrower`, `interest`, `amount`, `total_reimbursement`, `due_time`, `reference`, `status`) VALUES
+(1, 1, 1, '123123', '12345', 10, 1000000, 1100000, '2023-04-25', 'A123BC', 'PAIDOFF'),
+(2, NULL, 1, NULL, '123123123', 10, 1000000, 10000000, NULL, 'e736db01-7341-475b-b60c-799497b21172', 'REQUESTED'),
+(3, NULL, 1, NULL, '123123123', 10, 1000000, 1100000, NULL, '3b6135d6-eaba-455f-acd0-0c416960d972', 'REQUESTED');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `travel_history`
+--
+
+CREATE TABLE `travel_history` (
   `id` int(11) NOT NULL,
   `name` varchar(40) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -58,6 +88,14 @@ CREATE TABLE `payment_history` (
   `total` int(11) NOT NULL,
   `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `travel_history`
+--
+
+INSERT INTO `travel_history` (`id`, `name`, `email`, `account_number`, `package_name`, `count`, `total`, `status`) VALUES
+(1, 'Hugo', 'hugo@test.com', '123123123', 'Jalan jalan jogja', 2, 600000, 'SUCCESS'),
+(2, 'Hugo', 'hugo@test.com', '123123123', 'Jalan jalan jogja', 2, 600000, 'SUCCESS');
 
 -- --------------------------------------------------------
 
@@ -89,15 +127,16 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(40) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `pin` varchar(10) NOT NULL
+  `pin` varchar(10) NOT NULL,
+  `role` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `email`, `pin`) VALUES
-(1, 'Hugo', 'hugo@test.com', '123456');
+INSERT INTO `user` (`id`, `name`, `email`, `pin`, `role`) VALUES
+(1, 'Hugo', 'hugo@test.com', '123456', '');
 
 --
 -- Indexes for dumped tables
@@ -110,9 +149,15 @@ ALTER TABLE `account`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `payment_history`
+-- Indexes for table `lending_product`
 --
-ALTER TABLE `payment_history`
+ALTER TABLE `lending_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `travel_history`
+--
+ALTER TABLE `travel_history`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -138,10 +183,16 @@ ALTER TABLE `account`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `payment_history`
+-- AUTO_INCREMENT for table `lending_product`
 --
-ALTER TABLE `payment_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `lending_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `travel_history`
+--
+ALTER TABLE `travel_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `travel_package`
